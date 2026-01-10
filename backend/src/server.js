c48bbda130f272a4e946e1bdeb0162b4856ca734
Muhammad Ameen <muhammedameen9655@gmail.com>
2026-01-10 20:40:35 +0530

    change in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,9 +27,9 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
